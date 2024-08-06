3f7dce80ef4d3109c0a3c50456d210f651f7e839
dynames44 <dynames.n44@gmail.com>
2024-08-06 13:09:45 +0900

    예시추가

--- typeExam/dicTypeExam.py
+++ typeExam/dicTypeExam.py
@@ -8,7 +8,7 @@ print("생성 --------------------------------------------")
 print("dictData1:::", dictData1)
 print("dictData2:::", dictData2)
 print("dictData3:::", dictData3)
-print("")
+print()
 
 # 딕셔너리 항목추가
 dictData3["seqId"] = "0003" #딕셔너리 [키] = 값
@@ -20,13 +20,13 @@ print("딕셔너리 데이터 접근 방법 ------------------------------------
 print("Get Method:::", dictData3.get("seqId")) #get 메서드를 사용하여 접근
 print("Get Key Value:::", dictData3["name"]) #키값 사용하여 접근
 print("Data Get Default::", dictData3.get("test","nope")) #딕셔너리에 해당 키가 없을때 기본값 설정
-print("")
+print()
 
 # 딕셔너리 키 존재 여부 확인
 print("딕셔너리 키 존재 여부 확인 --------------------------------------------")
 print("Key Exist:::", "seqId" in dictData3) #딕셔너리에 있는 Key : True
 print("Key Not Exist:::", "test" in dictData3) #딕셔너리에 없는 Key : False
-print("")
+
 
 # 딕셔너리 메서드 활용
 print("딕셔너리 메서드 #1 --------------------------------------------")
@@ -43,12 +43,22 @@ if dictData4:
     print("dict Not Empty")
 else:
     print("dict Empty")
-print("")
+print()
 
 print("딕셔너리 메서드 #2 --------------------------------------------")
 dictData5 = dictData3.copy() # 딕셔너리 복사
+
 print("딕셔너리 Key 반환:::", dictData5.keys())
 print("딕셔너리 Value 반환:::", dictData5.values())
 print("딕셔너리 Key/Value 반환:::", dictData5.items())
 print("딕셔너리 항목 list 변환:::", list(dictData5.keys()))
 print("딕셔너리 Key/Value list 변환:::", type(list(dictData5.items())[0])) #items은 튜플 type로 반환 된다.
+
+
+print("그 외 --------------------------------------------")
+listData.append(dictData1)
+listData.append(dictData2)
+dictData5["listData"] = listData #딕셔너리에 리스트 데이터를 받아서 쓸수 있다.
+
+print("listData:::", listData)
+print("dictData5:::", dictData5)
